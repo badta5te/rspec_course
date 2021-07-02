5d99da2816bdf3fac7ed237f96f1ec6ea593e22d
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-07-03 00:16:25 +0300

    first tests

---
+++ lib/card.rb
@@ -0,0 +1,14 @@
+class Card
+  attr_reader :suit, :rank
+
+  def initialize(suit:, rank:)
+    @suit = suit
+    @rank = case rank
+            when :jack then 11
+            when :queen then 12
+            when :king then 13
+            else rank
+            end
+  end
+
+end
