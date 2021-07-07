5b90582a538f0c2a9e9e1a92ab85a5e46efa994f
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-07-07 22:22:20 +0300

    implement custom matcher

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,7 +3,7 @@
 require 'card'
 
 class Deck
-  RANKS = (7..10).to_a + %i[jack queen king ace]
+  RANKS = (6..9).to_a + %i[jack queen king ace]
 
   SUITS = %i[hearts clubs diamonds spades].freeze
 
