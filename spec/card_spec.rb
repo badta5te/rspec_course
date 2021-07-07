58e08fc3823793ef8600207885b77eda13bbaef9
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-07-07 20:09:06 +0300

    raise rewritten for the expect

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'set'
 require 'card'
 
-describe Card do
+describe Card, :unit do
   def card(params = {})
     defaults = {
       suit: :hearts,
@@ -10,12 +12,13 @@ describe Card do
 
     Card.build(*defaults.merge(params).values_at(:suit, :rank))
   end
+
   it 'has a suit' do
-    raise unless card(suit: :spades).suit == :spades
+    expect(card(suit: :spades).suit).to eq(:spades)
   end
 
   it 'has a rank' do
-    raise unless card(rank: 4).rank == 4
+    expect(card(rank: 4).rank).to eq(4)
   end
 
   context 'equality' do
@@ -25,21 +28,21 @@ describe Card do
       let(:other) { card(suit: :spades, rank: 4) }
 
       it 'is equal' do
-        raise unless subject == other
+        expect(subject).to eq(other)
       end
 
       it 'is hash equal' do
-        raise unless Set.new([subject, other]).size == 1
+        expect(Set.new([subject, other]).size).to eq(1)
       end
     end
 
     shared_examples_for 'an unequal card' do
       it 'is not equal' do
-        raise unless subject != other
+        expect(subject).not_to eq(other)
       end
 
       it 'is not hash equal' do
-        raise unless Set.new([subject, other]).size == 2
+        expect(Set.new([subject, other]).size).to eq(2)
       end
     end
 
@@ -58,28 +61,19 @@ describe Card do
 
   describe 'a jack' do
     it 'ranks higher than a 10' do
-      lower = card(rank: 10)
-      higher  = card(rank: :jack)
-
-      raise unless higher.rank > lower.rank
+      expect(card(rank: 10).rank).to be < card(rank: :jack).rank
     end
   end
 
   describe 'a queen' do
     it 'ranks higher than a jack' do
-      lower = card(rank: :jack)
-      higher  = card(rank: :queen)
-
-      raise unless higher.rank > lower.rank
+      expect(card(rank: :jack).rank).to be < card(rank: :queen).rank
     end
   end
 
   describe 'a king' do
     it 'ranks higher than a queen' do
-      lower = card(rank: :queen)
-      higher  = card(rank: :king)
-
-      raise unless higher.rank > lower.rank
+      expect(card(rank: :queen).rank).to be < card(rank: :king).rank
     end
   end
 end
